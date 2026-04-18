b6ae8cdf6e6c29bb44d39dc40efb9a5508025de2
N-Smillie <dominategamehs@gmail.com>
2026-04-18 08:59:23 +0100

    readied code for deployment

--- golf_tracker/settings.py
+++ golf_tracker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b2#ag($t-0pd))urixgoqo+6w0#7*8^g5)bg)#d7e3b7iqe5qb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
