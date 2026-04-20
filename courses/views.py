f3a7767a8842beedfc28fd2abd91a151a9ef7ee4
N-Smillie <dominategamehs@gmail.com>
2026-04-20 15:09:18 +0100

    Implement course list and detail views with slug-based routing

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Course
 
 # Create your views here.
 def course_list(request):
-    return render(request, 'courses/course_list.html')
+    courses = Course.objects.all()
+
+    return render(request, 'courses/course_list.html', {
+        'courses': courses
+    })
+
+def course_detail(request, slug):
+    course = get_object_or_404(Course, slug=slug)
+
+    return render(request, 'courses/course_detail.html', {
+        'course': course
+    })
