852b8ddfdeed51f748c516738ebf4bd181530c47
N-Smillie <dominategamehs@gmail.com>
2026-04-20 10:08:26 +0100

    created Course and Hole model in courses/models.py

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+class Hole(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='holes')
+    number = models.IntegerField()
+    par = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.course.name} - Hole {self.number}"
