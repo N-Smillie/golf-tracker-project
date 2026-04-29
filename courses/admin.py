08cc31008fe4cd473d07dfd3dbace0b796136221
N-Smillie <dominategamehs@gmail.com>
2026-04-29 09:37:24 +0100

    Improved course admin with inline hole editing and filtering

--- courses/admin.py
+++ courses/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 from .models import Course, Hole
 
 # Register your models here.
-admin.site.register(Course)
-admin.site.register(Hole)
+
+class HoleInline(admin.TabularInline):
+    model = Hole
+    extra = 0
+    ordering = ('number',)
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [HoleInline]
+
+@admin.register(Hole)
+class HoleAdmin(admin.ModelAdmin):
+    list_display = ('course', 'number', 'par')
+    list_filter = ('course',)
+    ordering = ('course', 'number')
