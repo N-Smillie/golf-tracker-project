8db42e35739357ec0a0d044f7d4cab869ce57c56
N-Smillie <dominategamehs@gmail.com>
2026-05-04 10:35:16 +0100

    added scorecard layout to course detail page and fixed view to pass hole data to template

--- courses/views.py
+++ courses/views.py
@@ -12,6 +12,9 @@ def course_list(request):
 def course_detail(request, slug):
     course = get_object_or_404(Course, slug=slug)
 
+    holes = course.holes.all().order_by('number')
+
     return render(request, 'courses/course_detail.html', {
-        'course': course
+        'course': course,
+        'holes': holes
     })
