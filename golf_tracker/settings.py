f6fe298ab124bdf78c0e5eadfed9f9f30a5026ad
N-Smillie <dominategamehs@gmail.com>
2026-05-14 17:26:22 +0100

    fix static files

--- golf_tracker/settings.py
+++ golf_tracker/settings.py
@@ -158,7 +158,11 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
